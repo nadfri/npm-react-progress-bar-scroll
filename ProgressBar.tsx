441fdfcc11e9e1436e0323f9ca646490603cdb1a
Nader Frigui <nadfri@gmail.com>
2022-02-08 13:42:42 +0100

    Add example files

--- ProgressBar.tsx
+++ ProgressBar.tsx
@@ -1,21 +1,14 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 interface Props {
   color1?: string;
   color2?: string;
   height?: string;
-  position?: React.CSSProperties['position'];
+  position?: React.CSSProperties["position"];
 }
 
-export function ProgressBar({
-  color1 = 'gray',
-  color2 = 'gold',
-  position = 'fixed',
-  height = '4px',
-}: Props) {
+export function ProgressBar({ color1, color2, position, height }: Props) {
   const progressBar_ref = useRef<HTMLDivElement>(null);
-
-
 
   const handleScroll = () => {
     const page = document.documentElement; //element HTML
@@ -23,41 +16,38 @@ export function ProgressBar({
     let visibleHeight = page.clientHeight; //Height visible
     let scrolling = page.scrollTop; //size of scroll
     let max = totalHeight - visibleHeight;
-    progressBar_ref.current!.style.width =
-      Math.floor((scrolling / max) * 100) + '%'; //width in %
+    progressBar_ref.current!.style.width = Math.floor((scrolling / max) * 100) + "%"; //width in %
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const positions = ['fixed', 'absolute', 'relative', "sticky"];
-
-  color1 = (typeof color1 === "string")? color1 : "gray";
-  color2 = (typeof color2 === "string")? color2 : "gold";
-  height = (typeof height === "string")? height : "4px";
-  position = (!positions.includes(position))? "fixed" : position;
-
-  console.log(color1);
+  //Defaults values
+  const positions : React.CSSProperties["position"][] = ["fixed", "absolute", "relative", "sticky","static"];
+  color1 = typeof color1 === "string" ? color1 : "#808080";
+  color2 = typeof color2 === "string" ? color2 : "#ffd700";
+  height = typeof height === "string" ? height : "4px";
+  position = !positions.includes(position) ? "fixed" : position;
 
   const styles: { [key: string]: React.CSSProperties } = {
     container: {
       position,
       height,
       backgroundColor: color1,
-      zIndex: '999',
-      width: '100%',
-      top: '0',
-      left: '0',
+      zIndex: "999",
+      width: "100%",
+      top: "0",
+      left: "0",
     },
 
     progressBar: {
       backgroundColor: color2,
-      transition: 'all 0.5s ease-out',
-      width: '0%',
-      height: '100%',
+      transition: "all 0.5s ease-out",
+      width: "0%",
+      height: "100%",
     },
   };
 
--- example/src/App.js
+++ example/src/App.js
@@ -1,19 +1,70 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { ProgressBar } from "@nadfri/react-scroll-progress-bar";
 
 function App() {
+  const [color1, setColor1] = useState("#808080");
+  const [color2, setColor2] = useState("#ffd700");
+  const [height, setHeight] = useState("4px");
+  const [position, setPosition] = useState("fixed");
 
-  const [color1, setColor1] = useState("");
-  const [color2, setColor2] = useState("");
-  const [height, setHeight] = useState("");
-  const [position, setPosition] = useState("");
+  useEffect(() => {
+    setTimeout(
+      () =>
+        window.scrollTo({
+          top: 200,
+        }),
+      500
+    );
+  }, []);
 
+  return (
+    <div className="App">
+      <ProgressBar color1={color1} color2={color2} height={height} position={position} />
 
+      <h3 style={{ position: "fixed" }}>BODY</h3>
+      <h3>APP with position</h3>
 
-  return <div className="App">
-    <ProgressBar color1={color1} color2={color2} height={height} position={position}/>
-  </div>;
+      <form>
+        <label style={{ backgroundColor: color1 }}>
+          Color1
+          <input
+            type="color"
+            onChange={(e) => setColor1(e.target.value)}
+            value={color1}
+          />
+        </label>
+        <label style={{ backgroundColor: color2 }}>
+          Color2
+          <input
+            type="color"
+            onChange={(e) => setColor2(e.target.value)}
+            value={color2}
+          />
+        </label>
+        <label>
+          Height
+          <input
+            type="range"
+            min="1"
+            max="15"
+            defaultValue={4}
+            onChange={(e) => setHeight(e.target.value + "px")}
+          />{" "}
+          {height}
+        </label>
+        <label>
+          Position
+          <select onChange={(e) => setPosition(e.target.value)} value={position}>
+            <option value="fixed">fixed</option>
+            <option value="absolute">absolute</option>
+            <option value="relative">relative</option>
+            <option value="static">static</option>
+            <option value="sticky">sticky</option>
+          </select>
+        </label>
+      </form>
+    </div>
+  );
 }
-
 export default App;
